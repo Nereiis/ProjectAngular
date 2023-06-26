816a9c31b8754a8e7e97f503c168e4b105e32291
Nereiis <nerearegsan.pro@gmail.com>
2023-06-26 13:56:32 +0200

    Carousel home

--- AgenciaViajes/src/app/shared/navbar/navbar.component.ts
+++ AgenciaViajes/src/app/shared/navbar/navbar.component.ts
@@ -6,19 +6,4 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  title = 'Viajar es Fácil'
-
-  navbg:any;
-  @HostListener('document:scroll') scrollover(){
-    console.log(document.body.scrollTop, 'scrolllength#');
-    
-    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      this.navbg = {
-        'background': 'linear-gradient(to bottom right, #ffffff)'
-        
-      }
-    } else {
-      this.navbg
-    }
-  }
 }
