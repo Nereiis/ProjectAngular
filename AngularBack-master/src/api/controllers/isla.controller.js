bf8d69a1766c3f643396fbcbd468682e52657b0f
RodrigoAnguloSantamaria <126025462+RodrigoAnguloSantamaria@users.noreply.github.com>
2023-06-26 15:07:36 +0200

    get de islas sin paginacion

--- AngularBack-master/src/api/controllers/isla.controller.js
+++ AngularBack-master/src/api/controllers/isla.controller.js
@@ -1,60 +1,60 @@
 const { deleteFile } = require('../../middlewares/delete.file');
 const Isla = require('../models/isla.model');
 
-// const getLibros = async(req,res) => {
-//     try {
-//         const allLibros = await Libro.find();
-//         return res.status(200).json(allLibros);
-//     } catch (error) {
-//         return res.status(500).json(error)
-//     }
-// }
-
-
-//GET PAGINADO
 const getIslas = async(req,res) => {
     try {
-        //Recoger querys de numero de pagina(page) y limite por pagina(limit)
-        let {page, limit} = req.query;
-        
-        //Contar el numero de elementos en mi coleccion
-        const numIslas = await Isla.countDocuments();
-        
-        //Si no está seteado seteo el limite a 10
-        limit = limit ? parseInt(limit) || 10 : 10;
-
-        //Comprobar el numero máximo de paginas dependiendo de mi limite
-        let numPages = numIslas%limit > 0 ? numIslas/limit + 1 : numIslas/limit;
-
-        //Si no está seteado seteo el numero de pagina a 1
-        page = page > numPages ? numPages : page < 1 ? 1 :  parseInt(page) || 1;
-        // if(page > numPages){
-        //     page = numPages;
-        // }else if(page < 1){
-        //     page = 1
-        // }else{
-        //     page = page
-        // }
-
-        // Calculo el salto(skip) que tengo que dar a mi find para empezar a partir del elemento que quiero
-        const skip = (page - 1) * limit;
-
-        const allIslas = await Isla.find().skip(skip).limit(limit);
-        const response = {
-            info: {
-                numLibros: numIslas,
-                page: page,
-                limit: limit,
-                nextPage: numPages >= page + 1 ? `/islas?page=${page + 1}&limit=${limit}` : null,
-                previusPage: page != 1 ? `/islas?page=${page - 1}&limit=${limit}` : null
-            },
-            results: allIslas
-        }
-        return res.status(200).json(response);
+        const allIslas = await Isla.find();
+        return res.status(200).json(allIslas);
     } catch (error) {
         return res.status(500).json(error)
     }
 }
+
+
+//GET PAGINADO
+// const getIslas = async(req,res) => {
+//     try {
+//         //Recoger querys de numero de pagina(page) y limite por pagina(limit)
+//         let {page, limit} = req.query;
+        
+//         //Contar el numero de elementos en mi coleccion
+//         const numIslas = await Isla.countDocuments();
+        
+//         //Si no está seteado seteo el limite a 10
+//         limit = limit ? parseInt(limit) || 10 : 10;
+
+//         //Comprobar el numero máximo de paginas dependiendo de mi limite
+//         let numPages = numIslas%limit > 0 ? numIslas/limit + 1 : numIslas/limit;
+
+//         //Si no está seteado seteo el numero de pagina a 1
+//         page = page > numPages ? numPages : page < 1 ? 1 :  parseInt(page) || 1;
+//         // if(page > numPages){
+//         //     page = numPages;
+//         // }else if(page < 1){
+//         //     page = 1
+//         // }else{
+//         //     page = page
+//         // }
+
+//         // Calculo el salto(skip) que tengo que dar a mi find para empezar a partir del elemento que quiero
+//         const skip = (page - 1) * limit;
+
+//         const allIslas = await Isla.find().skip(skip).limit(limit);
+//         const response = {
+//             info: {
+//                 numLibros: numIslas,
+//                 page: page,
+//                 limit: limit,
+//                 nextPage: numPages >= page + 1 ? `/islas?page=${page + 1}&limit=${limit}` : null,
+//                 previusPage: page != 1 ? `/islas?page=${page - 1}&limit=${limit}` : null
+//             },
+//             results: allIslas
+//         }
+//         return res.status(200).json(response);
+//     } catch (error) {
+//         return res.status(500).json(error)
+//     }
+// }
 const getIslaById = async(req,res)=>{
     try {
         const {id}=req.params;
